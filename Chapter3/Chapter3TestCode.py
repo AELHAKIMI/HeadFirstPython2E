0fdf07a2848c5558c83118d420e82f44a9d1f0b0
AELHAKIMI <ayo-voi@hotmail.com>
2018-07-19 13:51:04 +0100

    Work 18/07/2018

--- Chapter3/Chapter3TestCode.py
+++ Chapter3/Chapter3TestCode.py
@@ -1,3 +1,22 @@
-vowels = ('a', 'e', 'i', 'o', 'u')
-print(type(vowels))
-print(vowels)
+import pprint 
+people ={}
+people["Ayoub"] = {
+    'Name':'Ayoub ELHAKIMI',
+    'Gender': 'Male',
+    'Occupation': 'Developer',
+    'Country': 'Morocco'
+}
+people["Abdelaziz"] ={
+    'Name': 'Abdelaziz Saissi',
+    'Gender': 'Male',
+    'Occupation': 'Technicien',
+    'Country': 'Morocco'
+}
+
+people["Mahmoud"] = {
+    'Name': 'Mahmoud Solhi',
+    'Gender': 'Male',
+    'Occupation': 'Mathematician',
+    'Country': 'Morocco'
+}
+pprint.pprint(people)
