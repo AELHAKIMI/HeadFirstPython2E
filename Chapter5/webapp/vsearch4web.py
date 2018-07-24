1ca7d47652d52197b5dafff1031afdd52bd6e66e
AELHAKIMI <ayo-voi@hotmail.com>
2018-07-24 20:06:25 +0100

    Work 24/07/2018

--- Chapter5/webapp/vsearch4web.py
+++ Chapter5/webapp/vsearch4web.py
@@ -7,12 +7,20 @@ def hello() -> str:
     return " Heloo world from Flask!"
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', 
+                    the_phrase=phrase,
+                    the_letters= letters,
+                    the_title =title,
+                    the_results = results,)
+    
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', 
                              the_title='Welcome to search4letters on the web!')
-
 app.run(debug=True)
