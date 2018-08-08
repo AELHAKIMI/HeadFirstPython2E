d6c4f5a5eaf17457798c59ccc67008a979e6dd46
AELHAKIMI <ayo-voi@hotmail.com>
2018-08-08 13:52:37 +0100

    Work 08/08/2018

--- Chapter10/simple_webapp.py
+++ Chapter10/simple_webapp.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
+app.secret_key = "TheSecretKey"
 
 @app.route('/')
 def hello() -> str:
     return 'Hello from the simple webapp.'
+
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'you are now logged in'
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'you are currently logged in'
+    else:
+        return 'you are Not logged in'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'you are now logged out'
+        
+
 @app.route('/page1')
 def page1() -> str:
     return 'This is page 1.'
